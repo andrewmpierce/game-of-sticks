4953b2e200cce35484c57d744aa34c3d152dda89
apierce1992 <apierce1992@gmail.com>
2015-09-08 13:23:36 -0400

    Have a functional two player game, needs refactoring

--- game-of-sticks.py
+++ game-of-sticks.py
@@ -1,8 +1,28 @@
-def set_up_game():
+
+def set_up():
     player1 = input("Player 1, what's your name?\n")
     player2 = input("Player 2, what's your name?\n")
-    sticks = input("How many sticks are we playing with? Must be int.\n")
-    return player1, player2, sticks
+    return (player1, player2)
+
+def game_loop(players):
+    sticks = int(input("How many sticks are we playing with? Must be int.\n"))
+    while sticks > 0:
+        take_p1 = int(input("{}, how many sticks? Pick 1-3\n".format(players[0])))
+        sticks = sticks - take_p1
+        if sticks <= 0:
+            print("You took the last stick! You lose, {}".format(player[0]))
+            break
+        print("There are {} sticks left.".format(sticks))
+        take_p2 = int(input("{}, how many sticks? Pick 1-3\n".format(players[1])))
+        sticks = sticks - take_p2
+        if sticks <= 0:
+            print("You took the last stick! You lose, {}".format(players[1]))
+            break
+        print("There are {} sticks left.".format(sticks))
+
+
 
 def main():
-    set_up_game()
+    game_loop(set_up())
+
+main()
