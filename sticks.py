b12afdf065065b811b913a065355639ba0878353
apierce1992 <apierce1992@gmail.com>
2015-09-08 18:48:10 -0400

    fixed a few small bugs

--- sticks.py
+++ sticks.py
@@ -1,16 +1,18 @@
 import random
 
-ai_dict = {1:[1], 2:[1, 2]}
-key_start = 3
-for x in range(18):
-    ai_dict.update({key_start:[1, 2, 3]})
-    key_start += 1
+def ai_dict():
+    ai_dict = {1:[1], 2:[1, 2]}
+    key_start = 3
+    for x in range(18):
+        ai_dict.update({key_start:[1, 2, 3]})
+        key_start += 1
+    return ai_dict
 
 
 def get_sticks(player):
     get_pick = False
     while get_pick == False:
-        pick = int(input("How many sticks, {}?\n".format(player)))
+        pick = int(input("How many sticks, {}? Pick 1-3.\n".format(player)))
         if pick >= 1 and pick <= 3:
             get_pick = True
         else:
@@ -32,7 +34,7 @@ def who_lost(player, sticks):
         pass
 
 
-def update_AI(dictionary, choices, won):
+def update_AI(dictionary, choices, won, player1):
     updated_dict = dictionary
     if won == True:
         for x in updated_dict:
@@ -45,10 +47,9 @@ def update_AI(dictionary, choices, won):
                 if comp_picks == x:
                     updated_dict[comp_picks].remove(choices.get(comp_picks))
     print(updated_dict)
-    game_loop_ai(updated_dict)
+    game_loop_ai(updated_dict, player1)
 
-def game_loop_ai(dictionary):
-    player1 = input("What is your name?\n")
+def game_loop_ai(dictionary, player1):
     sticks = int(input("How many sticks? Pick between 1- 20\n"))
     comp_choices = {}
     while is_game_over(sticks) == False:
@@ -64,12 +65,12 @@ def game_loop_ai(dictionary):
         loser = who_lost("Skynet", sticks)
         is_game_over(sticks)
     print("{} lost. Sucks to suck.".format(loser))
-    play_again = input("Do you want to play again? yes or no?")
+    play_again = input("Do you want to play again? Enter yes or no.\n")
     if play_again.lower() == 'yes':
         if loser is "Skynet":
-            update_AI(dictionary, comp_choices, False)
+            update_AI(dictionary, comp_choices, False, player1)
         else:
-            update_AI(dictionary, comp_choices, True)
+            update_AI(dictionary, comp_choices, True, player1)
     else:
         print("Okay, maybe another time.")
 
@@ -93,7 +94,8 @@ def game_loop_2player():
 def main():
     choice = int(input("Do you want to play an AI or play with a human? Enter 1 for AI, 2 for human.\n"))
     if choice == 1:
-        game_loop_ai(ai_dict)
+        player1 = input("What is your name?\n")
+        game_loop_ai(ai_dict(), player1)
     elif choice == 2:
         game_loop_2player()
 
