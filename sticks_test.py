9e21a163a5fc302b458a50f6e433662ad4653dc3
apierce1992 <apierce1992@gmail.com>
2015-11-19 22:53:54 -0500

    added in extra test case

--- sticks_test.py
+++ sticks_test.py
@@ -7,10 +7,11 @@ class TestSticks(unittest.TestCase):
         self.assertTrue(is_game_over(0))
         self.assertTrue(is_game_over(-1))
         self.assertFalse(is_game_over(3))
+        self.assertTrue(is_game_over(-1))
 
     def test_who_lost(self):
         self.assertEqual(who_lost("Bill", 1), "Bill")
         self.assertEqual(who_lost("Bill", 0), "Bill")
-
+    
 if __name__ == '__main__':
     unittest.main()
